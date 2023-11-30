609bb602a23f1b330fd6c5802a035186d0d79655
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-11-30 13:57:58 -0500

    added photos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,42 @@
-import React from 'react'
+import React from "react";
 
 function Projects() {
   return (
     <section id="projects">
-    <div className="project-section">
-      <div className="project-container">
-        <div className="main-img">
-          <a href="https://quiz.com">
-            <img src="assets/quiz.jpg" alt="quiz-img" />
-          </a>
-        </div>
+      <div className="project-section">
+        <div className="project-container">
+          <div className="main-img">
+            <a href="https://quiz.com">
+              <img src="./assets/quizapp.jpg" alt="quiz-img" />
+            </a>
+          </div>
 
-        <div className="secondary-img">
-          <a className="secondary-img-link" href="https://weather.com">
-            <img className="" src="assets/weather.jpg" alt="workday app" />
-          </a>
-          <a
-            className="secondary-img-link"
-            href="https://www.countdown.com"
-          >
-            <img
-              className=""
-              src="assets/countdown.jpg"
-              alt="password app"
-            />
-          </a>
+          <div className="secondary-img">
+            <a className="secondary-img-link" href="https://weather.com">
+              <img className="" src="assets/weather.jpg" alt="weather app" />
+            </a>
+            <a className="secondary-img-link" href="https://www.countdown.com">
+              <img className="" src="./assets/Jate.png" alt="password app" />
+            </a>
+          </div>
+          <div className="secondary-img">
+            <a className="secondary-img-link" href="https://weather.com">
+              <img className="" src="assets/project-1.jpg" alt="weather app" />
+            </a>
+            <a className="secondary-img-link" href="https://www.countdown.com">
+              <img className="" src="./assets/readme.png" alt="password app" />
+            </a>
+          </div>
+          <div className="main-img">
+            <a href="https://quiz.com">
+              <img src="./assets/project-1.jpg" alt="quiz-img" />
+            </a>
+          </div>
+
         </div>
       </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
