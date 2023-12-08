87dc22ec4ca3239123cd9d61ba6f494dba7d8194
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-12-07 22:10:25 -0500

    added resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Resume from '/assets/Resume.pdf'
+import { Document, Page, pdfjs } from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 function Resume() {
   return (
-    <div>Resume</div>
+    <div className='res'>
+      <h2 className='heading'>Resume</h2>
+      <Document
+         file={resume}
+         onLoadError={console.error}
+         style={{ width: '100vw', height: 'auto'}}
+      >
+     <Page pageIndex={0}/>
+      </Document>
+    </div>
   )
 }
 
